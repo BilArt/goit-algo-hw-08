bed9e7fd2d252e9266bbaea3dc9d7c745f7c10c8
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-03-05 16:18:12 +0100

    added def prim_mst in class Graph

--- main.py
+++ main.py
@@ -21,3 +21,24 @@ class Graph:
                 min_index = v
 
         return min_index
+
+    def prim_mst(self):
+        key = [float('inf')] * self.V
+        parent = [None] * self.V
+        key[0] = 0
+        mst_set = [False] * self.V
+
+        parent[0] = -1
+
+        for cout in range(self.V):
+            u = self.min_key(key, mst_set)
+            mst_set[u] = True
+
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and not mst_set[v] and key[v] > self.graph[u][v]:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+
+        total_cost = sum(key)
+        return total_cost
+
