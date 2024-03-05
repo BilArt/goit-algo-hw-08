a98df3e4338140023f915a36acc24350fc8f60ba
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-03-05 16:14:20 +0100

    added def min_key in class Graph

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ class Graph:
         self.graph[u][v] = w
         self.graph[v][u] = w
 
+
+    def min_key(self, key, mst_set):
+        min_value = float('inf')
+        min_index = None
+
+        for v in range(self.V):
+            if key[v] < min_value and not mst_set[v]:
+                min_value = key[v]
+                min_index = v
+
+        return min_index
